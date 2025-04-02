74ee1ab13fb7bb5712008f6bee21874f4f218fc8
mayett515 <150403514+mayett515@users.noreply.github.com>
2025-04-02 12:22:59 +0200

    Update vite.config.js
    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: process.env.PORT || 10000
+  },
+  server: {
+    host: true,
+    port: process.env.PORT || 10000
+  }
 });
