25ee921529cd2576d9f0c0510e01e5af70d34e02
mayett515 <matthias.vmach@gmail.com>
2025-04-18 11:02:14 +0200

    DaisyUI and TailwindCss works

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from "daisyui";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
+  plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "cupcake", "synthwave"],
+    themes: ["dark"],
   },
 };
